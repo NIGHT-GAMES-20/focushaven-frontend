344f622c8d1492584e0bef5eac6253df0a777c2b
Abhinash Dey <night20games@gmail.com>
2025-05-01 22:40:13 +0530

    dasshboard for admin added

---
+++ src/components/Dashboard/Dashboard.vue
@@ -0,0 +1,43 @@
+<template>
+  <div v-if="isAuthenticated && isAdmin" >
+    <button @click="updateDB" >Update Notes From DB</button>
+  </div>
+</template>
+
+<script setup>
+import { ref, onMounted } from 'vue'
+
+const isAuthenticated = ref(false)
+const isAdmin = ref(false)
+
+async function checkAuth() {
+    try {
+        const response = await fetch(`${import.meta.env.VITE_BACKEND_URL}/login/protected/admin-status`, {
+        method: 'GET',
+        credentials: 'include'
+        });
+        const data = await response.json()
+        isAuthenticated.value = !!data.success
+        isAdmin.value = data.admin|| false
+    } catch {
+        isAuthenticated.value = false
+    }
+}
+
+onMounted(() => {
+    checkAuth()
+})
+
+async function updateDB() {
+    try {
+        const response = await fetch(`${import.meta.env.VITE_BACKEND_URL}/notes/update`, {
+            method: 'POST',
+            credentials: 'include'
+        });
+        const data = await response.json()
+        console.log('Update Notes:', data)
+    } catch (error) {
+        console.error('Failed to update notes:', error)
+    }
+}
+</script>
