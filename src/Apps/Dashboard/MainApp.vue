347adef8010c20c295ec9055b9c098770a020fb0
Abhinash Dey <night20games@gmail.com>
2025-05-02 08:48:27 +0530

    Dashboard For Admin Changed

--- src/Apps/Dashboard/MainApp.vue
+++ src/Apps/Dashboard/MainApp.vue
@@ -1,11 +1,13 @@
 <script setup>
     import Footer from "../../components/Footer/Footer.vue"
     import Header from "../../components/Header/Header.vue";
+    import Dashboard from "../../components/Dashboard/Dashboard.vue";
 </script>
 
 <template>
     <div class="mainApp">
         <Header />
+        <Dashboard />
         <p>Heloo From the Dashboard</p>
         <Footer />
     </div>
