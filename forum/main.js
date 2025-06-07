9edc2aa6057a2a41fa8ef7553afec4ffd70d1e10
NIGHT-GAMES-20 <night20games@gmail.com>
2025-06-07 11:41:47 +0000

    Testing questions Page

--- forum/main.js
+++ forum/main.js
@@ -2,6 +2,4 @@ import { createApp } from 'vue';
 import { router } from './router';
 import QuestionApp from './QuestionView.vue';
 
-const app = createApp(QuestionApp);
-app.use(router);
-app.mount('#app');
+createApp(QuestionApp).use(router).mount('#question-root');
