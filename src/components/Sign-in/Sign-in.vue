09c70798cbf2b96e27e85ae48f35437c36d6562c
Abhinash Dey <night20games@gmail.com>
2025-06-17 16:29:17 +0530

    Signin Page Stucture Added

--- src/components/Sign-in/Sign-in.vue
+++ src/components/Sign-in/Sign-in.vue
@@ -5,11 +5,13 @@
     <label>
       Name:
       <input v-model="name" type="text" placeholder="Enter Name" :class="styles.usernameInput" />
+      <span :class="styles.errMsg">{{ nameErrMsg }}</span>
     </label>
 
     <label>
       Username:
       <input v-model="username" type="text" placeholder="Enter username" :class="styles.usernameInput" />
+      <span :class="styles.errMsg">{{ usernameErrMsg }}</span>
     </label>
 
     <label>
@@ -20,7 +22,11 @@
           <span :class="styles.tooltipText">Account Rcovery Will Not Be Available If Email is not Added</span>
         </span>
       </span>
-      <input v-model="email" type="email" placeholder="Enter Email" :class="styles.usernameInput" />
+      <div :class="styles.passwordRow">
+        <input v-model="email" type="email" placeholder="Enter Email" :class="styles.passwordInput" :disabled="emailVerificationStatus" />
+        <button type="button" @click="VerifyEmail" :class="[styles.emailVerifyBtn, emailVerificationStatus ? styles.verifiedEmail : '' ]" :disabled="emailVerificationStatus" >{{ emailVerificationBtn }}</button>
+      </div> 
+      <span :class="styles.errMsg">{{ emailErrMsg }}</span>
     </label>
 
     <label>
@@ -31,6 +37,7 @@
           <img :src="currentImage" alt="Toggle Password" :class="styles.viewPassImg" />
         </button>
       </div>
+      <span :class="styles.errMsg">{{ passwordErrMsg }}</span>
     </label>
 
     <label>
@@ -38,6 +45,7 @@
       <div :class="styles.passwordRow">
         <input :type="showPassword ? 'text' : 'password'" v-model="CNFpassword" placeholder="Enter password" :class="styles.passwordInput"/>
       </div>
+      <span :class="styles.errMsg">{{ CNFpasswordErrMsg }}</span>
     </label>
 
     <div :class="styles.buttonRow">
@@ -50,11 +58,12 @@
 
   
 <script setup>
-  import { ref, onMounted } from 'vue'
+  import { ref, onMounted, watch } from 'vue'
   import MD5 from 'crypto-js/md5'
   import imageA from './assets/view.png'
   import imageB from './assets/hide.png'
   import styles from './Sign-in.module.css'
+  import axios from 'axios'
 
   const currentImage = ref(imageA)
   
@@ -66,6 +75,131 @@
   const showPassword = ref(false)
   const responseMessage = ref('')
   const isAuthenticated = ref(false)
+  const usernameErrMsg = ref('')
+  const nameErrMsg = ref('')
+  const emailErrMsg = ref('')
+  const passwordErrMsg = ref('')
+  const CNFpasswordErrMsg = ref('')
+  const emailVerificationStatus = ref(false)
+  const emailVerificationBtn = ref('Verify')
+
+
+  watch(username, async () => {
+    const val = username.value
+    usernameErrMsg.value = ''
+
+    // Character check
+    const isValidString = validateUsernameString(val)
+    if (!isValidString) {
+      usernameErrMsg.value = 'Username must contain only alphanumeric characters and underscores'
+      return
+    }
+
+
+    // Length checks
+    if (val.length < 3) {
+      usernameErrMsg.value = 'Username must be at least 3 characters long'
+      return
+    }
+
+    if (val.length > 20) {
+      usernameErrMsg.value = 'Username must be less than 20 characters long'
+      return
+    }
+
+    // Availability check
+    const isAvailable = await validateUsername(val)
+    if (!isAvailable) {
+      usernameErrMsg.value = 'Username already in use'
+      return
+    }
+
+    // All good
+    usernameErrMsg.value = ''
+  })
+
+  //Name validation
+  watch(name, async () => {
+    const val = name.value
+    nameErrMsg.value = ''
+
+    // Character check
+    const isValidString = validateNameString(val)
+    if (!isValidString) {
+      nameErrMsg.value = 'Name must contain only alphanumeric characters'
+      return
+    }
+
+    // Length checks
+    if (val.length < 3) {
+      nameErrMsg.value = 'Name must be at least 3 characters long'
+      return
+    }
+
+    // All good
+    nameErrMsg.value = ''
+  })
+
+  // Email validation
+  watch(email, async () => {
+    const val = email.value
+    emailErrMsg.value = ''
+
+    // Character check
+    const isValidString = validateEmailString(val)
+    if (!isValidString) {
+      emailErrMsg.value = 'Enter A Valid Email Address'
+      return
+    }
+
+    // Length checks
+    if (val.length < 3) {
+      emailErrMsg.value = 'Email must be at least 3 characters long'
+      return
+    }
+
+    // All good
+    emailErrMsg.value = ''
+  })
+
+
+  function validateEmailString(email) {
+    const re = /^[^\s@]+@[^\s@]+\.[^\s@]+$/
+    return re.test(email)
+  }
+
+  function validateUsernameString(val) {
+    return /^[a-zA-Z0-9_]+$/.test(val)
+  }
+
+  function validateNameString(val) {
+    return /^[a-zA-Z ]+$/.test(val)
+  }
+
+  async function validateUsername(username) {
+    try{
+      const response = await axios.get(`${import.meta.env.VITE_BACKEND_URL}/signin/usernameValidation`, {
+        params: { username },
+      })
+      const data = await response.json()
+      if (data.success) {
+        return data.isAvailable
+      }
+    } catch (error) {
+      console.error('Error validating username:', error)
+      return false
+    }  
+  }
+
+  async function VerifyEmail() {
+    if (!email.value) {
+      emailErrMsg.value = 'Please enter an email address'
+      return
+    }
+
+    emailVerificationStatus.value = true
+    emailVerificationBtn.value = 'Verified'
+  }
   
   function togglePassword() {
     showPassword.value = !showPassword.value
@@ -126,7 +260,5 @@
       responseMessage.value = `Failed To Fetch Response: ${error}`
     }
   }
-  
-  
+   
   </script>
-  
