fc45e0012be99a919aaecd350fcd7e4986396bf7
Abhinash Dey <night20games@gmail.com>
2025-08-11 22:12:40 +0530

    somethings Changed

--- src/components/Header/Header.vue
+++ src/components/Header/Header.vue
@@ -43,31 +43,15 @@
           <li><a href="/notes" :class="styles.links">Notes</a></li>
           <li><a href="https://focushaven-testportal.netlify.app/" :class="styles.links">Test Portal</a></li>
           <li><a href="/forum" :class="styles.links">Forum</a></li>
-          <div v-if="userStore.isLoggedIn" @click="toggleDropdown" :class="styles.userIdenticonContainer" ref="dropdownRef">
-            <img  alt="user Identicon" :src="userStore.profilePic" :class="styles.userIdenticon" ref="dropdownRef" /><ChevronDown :class="[styles.chevronIconDropown, { [styles.rotated]: isUserDropdownOpen }]" />
-          </div>
+          <li v-if="userStore.isLoggedIn"><a href="/dashboard" :class="styles.links">Dashboard</a></li>
+          <li v-if="userStore.isLoggedIn">
+            <a @click="logoutFunc" href="javascript:void(0)" style="margin-right: 10px;" :class="styles.links">Logout</a>
+          </li>
           <li v-else><a href="/login" :class="styles.links" >Login/Register</a></li>
-          <transition name="fade">
-            <div v-if="isUserDropdownOpen" :class="styles.userDropdown"  @click.stop >
-              <a href="/dashboard" :class="styles.dropdownElem">Dashboard</a>
-              <a @click="logoutFunc()" :class="styles.dropdownElem">Logout</a>
-            </div>
-          </transition>
-          
         </ul>
       </nav>
     </div>
   </header>
 </template>
 
-<style>
-  /* Fade in / out transition classes */
-  .fade-enter-active,
-  .fade-leave-active {
-    transition: opacity 0.25s ease;
-  }
-  .fade-enter-from,
-  .fade-leave-to {
-    opacity: 0;
-  }
-</style>
+
