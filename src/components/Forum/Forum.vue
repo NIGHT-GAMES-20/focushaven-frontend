b6a14154b05836abd6fe57812ff471366df69a1b
Abhinash Dey <night20games@gmail.com>
2025-08-14 15:41:13 +0530

    modularized secureFetch

--- src/components/Forum/Forum.vue
+++ src/components/Forum/Forum.vue
@@ -81,6 +81,7 @@
   import { ref , onMounted, onBeforeUnmount  } from 'vue';
   import Styles from './Forum.module.css';
   import { useUserStore } from '/stores/user.js'
+  import { secureFetch } from '../../scripts/forumSecureFetch';
   import { Trash2, ThumbsUp, ArrowLeft,ChevronDown } from 'lucide-vue-next';
   import QuestionAsk from '../Question/QuestionAsk.vue';
   import ForumLogo from '../../assets/ForumLogoTitle.png'
@@ -195,35 +196,6 @@
     fetchQuestions(currentPage.value); // Refresh the questions list after deletion
   }
 
-
-  async function getValidToken() {
-    let token = sessionStorage.getItem('formAuthToken');
-
-    if (!token || isTokenExpired(token)) {
-      const res = await fetch(`${import.meta.env.VITE_BACKEND_URL}/forum/auth/token`, { method: 'POST',headers: { 'Origin': window.location.origin } });
-      const data = await res.json();
-      sessionStorage.setItem('formAuthToken', data.token);
-      return data.token;
-    }
-
-    return token;
-  }
-
-  function isTokenExpired(token) {
-    if (!token) return true;
-    const payload = JSON.parse(atob(token.split('.')[1]));
-    return Date.now() >= payload.exp * 1000;
-  }
-
-  async function secureFetch(url, options = {}) {
-    const token = await getValidToken();
-    options.headers = {
-      ...options.headers,
-      Authorization: `Bearer ${token}`,
-      'Content-Type': 'application/json'
-    };
-    return fetch(url, options);
-  }
 
   onMounted(() => {
     userStore.fetchUser()
--- src/components/Question/QuestionView.vue
+++ src/components/Question/QuestionView.vue
@@ -35,35 +35,6 @@
     }
   }
 
-  async function getValidToken() {
-    let token = sessionStorage.getItem('formAuthToken');
-
-    if (!token || isTokenExpired(token)) {
-      const res = await fetch(`${import.meta.env.VITE_BACKEND_URL}/forum/auth/token`, { method: 'POST',headers: { 'Origin': window.location.origin } });
-      const data = await res.json();
-      sessionStorage.setItem('formAuthToken', data.token);
-      return data.token;
-    }
-
-    return token;
-  }
-
-  function isTokenExpired(token) {
-    if (!token) return true;
-    const payload = JSON.parse(atob(token.split('.')[1]));
-    return Date.now() >= payload.exp * 1000;
-  }
-
-  async function secureFetch11(url, options = {}) {
-    const token = await getValidToken();
-    options.headers = {
-      ...options.headers,
-      Authorization: `Bearer ${token}`,
-      'Content-Type': 'application/json'
-    };
-    return fetch(url, options);
-  }
-
   onMounted(() => {
     fetchData();
   });
