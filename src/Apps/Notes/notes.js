34e4c3032a8626e88542faa96df0178b6229457c
Abhinash Dey <night20games@gmail.com>
2025-08-31 14:33:29 +0530

    Spelling Fixed

--- src/Apps/Notes/notes.js
+++ src/Apps/Notes/notes.js
@@ -6,4 +6,4 @@ import piniaPluginPersistedstate from 'pinia-plugin-persistedstate'
 const pinia = createPinia()
 pinia.use(piniaPluginPersistedstate)
 
-createApp(App).use(pinna).mount("#root");
+createApp(App).use(pinia).mount("#root");
