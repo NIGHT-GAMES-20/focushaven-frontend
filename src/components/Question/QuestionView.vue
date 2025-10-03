0dbbb43e2a878132f8a0279aab0daf8e70f57b15
Abhinash Dey <night20games@gmail.com>
2025-10-03 13:19:37 +0530

    Answer Display added 3

--- src/components/Question/QuestionView.vue
+++ src/components/Question/QuestionView.vue
@@ -45,8 +45,8 @@
               <img :src="generateIdenticon(answer.user)" :alt="`Avatar of ${answer.user}`" :class="styles.userIdenticon" />
               <span>Answered by <strong>{{ answer.user }}</strong></span>
               <span> • {{ formatDate(answer.CreatedAt) }}</span>
-              <span v-if="answer.status == 'verified'"><CircleCheck color="green" :size="14" />Verified</span>
-              <span v-else-if="answer.status == 'unverified'"><CircleX color="red" :size="12" />Unverifed</span>
+              <span v-if="answer.status == 'verified'">|| <CircleCheck color="green" :size="14" /> Verified</span>
+              <span v-else-if="answer.status == 'unverified'">|| <CircleX color="red" :size="14" /> Unverifed</span>
             </div>
             <div :class="styles.answerUserControlBtns" >
               <Heart :color="answer.Liked ? 'red' : 'grey'" :size="20" :class="styles.userActions" />
